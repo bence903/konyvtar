031e157448ee7576059602f4e12aa9407ae84eef
bence903 <ujhelyibence@gmail.com>
2021-11-28 21:56:51 +0100

    Update konyvek.c

--- konyvek.c
+++ konyvek.c
@@ -33,46 +33,55 @@ char *kisbetu(char *str){
     return str;
 }
 //Új könyvek eltárolására szolgáló függvény
-konyvek *konyv_hozza(konyvek *p){
-
-    int n = 0;
-    int ev;
+konyvek *konyv_hozza(konyvek *p)
+{
+    int ev = 0;
     char cim[101];
     char szerzo[51];
     char tema[21];
 
-    konyvek *uj;
-    uj = (konyvek*) malloc(sizeof(konyvek));
-    uj->kov = p;
+    konyvek *uj = (konyvek*) malloc(sizeof(konyvek));
     printf("\nKérem a könyv adatait:\n");
     printf("\nCím: ");
-    fgetc(stdin);
 
+    //Könyv címe
+    fgetc(stdin);
     fgets(cim, 100, stdin);
-    strcpy(cim,ujSorKi(cim));
-    
+    cim[strcspn(cim, "\n")] = 0;
+
+    //Könyv szerzője
     printf("\nSzerző: ");
     fgets(szerzo, 50, stdin);
-    n = 0;
-    strcpy(szerzo,ujSorKi(szerzo));
+    szerzo[strcspn(szerzo, "\n")] = 0;
+
+    //Könyv kiadása
     printf("\nKiadás: ");
     scanf("%d",&ev);
+
+    //Könyv témája
     printf("\nTéma: ");
     fgetc(stdin);
     fgets(tema, 20, stdin);
-    strcpy(tema,ujSorKi(tema));
+    tema[strcspn(tema, "\n")] = 0;
+
     strcpy(uj->cim,cim);
     strcpy(uj->szerzo,szerzo);
     strcpy(uj->tema,tema);
     uj->kolcs = 0;
     uj->kiadas = ev;
-    while (p != NULL)
-        p = p->kov;
-    p = uj;
+    uj->kov = NULL;
+
+    if (p == NULL) {
+        p = uj;
+    } else {
+        konyvek *last = p;
+        while (last->kov != NULL) {
+            last = last->kov;
+        }
+        last->kov = uj;
+    }
 
     return p;
-    
-    
 }
 
 void konyv_torl(konyvek *lista){
